59ca5b009b4d6f2a1a1c1f8ed9780bf1de157409
bishal2059 <bishal.sap21@gmail.com>
2024-11-11 20:22:06 +0545

    cannonical mode disabled

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-#include<stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -10,11 +12,12 @@ void disableRawMode(){
 }
 
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
+  struct termios raw = orig_termios;
 
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -22,7 +25,13 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if(iscntrl(c)){
+        printf("%d\n",c);
+    }else{
+        printf("%d ('%c')\n",c,c);
+    }
+  };
   return 0;
 }
 
