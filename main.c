4e2d7e929ca945973044c39b47f8988c3e5c4a02
bishal2059 <bishal.sap21@gmail.com>
2024-11-11 20:07:23 +0545

    raw mode disable

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
+#include<stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+
+void disableRawMode(){
+   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void enableRawMode() {
   struct termios raw;
+
   tcgetattr(STDIN_FILENO, &raw);
+
   raw.c_lflag &= ~(ECHO);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
   enableRawMode();
   char c;
